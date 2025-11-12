e017c2d3e80af08bb8e5082bf33f86f519723e90
Caio Fernandes <caiovitfernandes@gmail.com>
2025-11-11 22:33:18 -0300

    teste

--- script.js
+++ script.js
@@ -39,7 +39,7 @@ btnAdicionar.addEventListener('click', async () => {
   const body = { nome };
 
   try {
-    const resp = await fetch(`${API_BASE}/addPlayer`, {
+    const resp = await fetch(`/addPlayer`, {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify(body)
